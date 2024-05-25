c7b936667f2c18327db205bfec8d6edea52b6e24
Kazuhiko-Gushiken <just.a.stem.nerd@gmail.com>
2024-05-25 15:26:47 -0700

    Update CompletionistTrophy.java

--- src/main/java/com/enginnx/AOEItems/blocks/CompletionistTrophy.java
+++ src/main/java/com/enginnx/AOEItems/blocks/CompletionistTrophy.java
@@ -2,15 +2,9 @@ package com.enginnx.AOEItems.blocks;
 
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
